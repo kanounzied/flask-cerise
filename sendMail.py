6c8d3ef33f45910f431327d952360d29b4aef5a4
ahmedjaabiri <71710960+ahmedjaabiri@users.noreply.github.com>
2020-10-09 11:19:04 +0100

    added gen pdf constat

--- sendMail.py
+++ sendMail.py
@@ -318,3 +318,81 @@ def sendPDFv(receiver_mail,pdf, text):
         # print('connected!')
         server.sendmail(sender_mail, receiver_mail, message.as_string())
         # print('message sent!')
+  def sendPDFc(receiver_mail,pdf, text):
+    port = 465  # For SSL
+    password = "Ds3X8afA"  # getpass.getpass("Type your password : ")
+    sender_mail = "artzy.proj@gmail.com"
+    # receiver_mail = "zied.kanoun6@gmail.com"
+    subject = "Constat d'accident automobile"
+
+    message = MIMEMultipart("alternative")
+    message['Subject'] = subject
+    message['From'] = sender_mail
+    message['To'] = receiver_mail
+    message["Bcc"] = receiver_mail  # Recommended for mass emails
+
+    # Create the plain-text and HTML version of your message
+
+    plain_text = """\
+    hi,
+    this message is for test,
+    to send contract ntification"""
+    # text assosciation : this is the contract of the client named client with the id id : this contract is (still not)
+    # paid
+    # text client : the contract is ready now and waiting to be paid if you want to modify it just log in and choose
+    # your contract if you have more than one
+    html = """\
+    <html>
+        <body style='color: black;'>
+            <h1>Hello , the accident report is here! </h1><br>
+            <p>"""+text+"""</p>
+            <p>    </p>
+        </body>
+    </html>
+    """
+
+    # Turn these into plain/html MIMEText objects
+
+    part1 = MIMEText(plain_text, "plain")
+    part2 = MIMEText(html, "html")
+
+    # Add HTML/plain-text parts to MIMEMultipart message
+    # The email client will try to render the last part first
+
+    message.attach(part1)
+    message.attach(part2)
+
+    ############### attachment part ##################
+
+    # filename = pdf_name  # In same directory as script
+
+    # Open PDF file in binary mode
+    # with open(filename, "rb") as attachment:
+    #     # Add file as application/octet-stream
+    #     # Email client can usually download this automatically as attachment
+    part = MIMEBase("application", "octet-stream")
+    #     # print(attachment.read())
+    part.set_payload(pdf)
+
+    # Encode file in ASCII characters to send by email
+    encoders.encode_base64(part)
+
+    # Add header as key/value pair to attachment part
+    part.add_header(
+        "Content-Disposition",
+        f"attachment; filename= {'constat.pdf'}",
+    )
+
+    # Add attachment to message and convert message to string
+    message.attach(part)
+
+    ############ end of attachment part ###############
+
+    # Create a secure SSL context
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+        server.login("artzy.proj@gmail.com", password)
+        # print('connected!')
+        server.sendmail(sender_mail, receiver_mail, message.as_string())
+        # print('message sent!')
